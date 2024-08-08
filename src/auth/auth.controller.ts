6b56d63840f6c2ef72baf0db455009c881508903
Sebastian <sebastian@coderlabs.co>
2024-08-08 14:52:05 -0400

    feat:: add openapi

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,21 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginResponseDto } from './dto/login-response.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(@Request() req): LoginResponseDto {
-    return this.authService.login(req.user);
+  login(@Body() loginUser: LoginUserDto): LoginResponseDto {
+    return this.authService.login(loginUser);
   }
 
   @Post('signup')
-  signup(@Request() req, @Body() createUser: CreateUserDto) {
-    console.log(createUser);
+  signup(@Body() createUser: CreateUserDto) {
     return this.authService.signUp(createUser);
   }
 }

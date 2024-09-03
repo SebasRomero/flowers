250959bcd8adca5bd87445efdf952a66731680f7
Sebastian <sebastian@coderlabs.co>
2024-09-03 00:01:17 -0400

    fix:: add email to clients

--- src/client/types/client.types.ts
+++ src/client/types/client.types.ts
@@ -4,6 +4,7 @@ import { BookingStatus } from 'src/booking/types/booking-status';
 export interface IClient {
   _id?: Types.ObjectId;
   name: string;
+  email: string;
   phone: string;
   orderNumber: string;
   status: BookingStatus;

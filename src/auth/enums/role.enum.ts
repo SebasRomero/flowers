66e2e8959f745e7f676eb97eee784fc1de075782
Sebastian <sebastian@coderlabs.co>
2024-09-27 17:17:21 -0400

    change:: enum, add new role owner

--- src/auth/enums/role.enum.ts
+++ src/auth/enums/role.enum.ts
@@ -1,10 +1,21 @@
 export enum Role {
   AGENT = 'agent',
   ADMIN = 'admin',
+  OWNER = 'owner',
 }
 
 export enum RoleAgent {
   AGENT = 'agent',
 }
 
+export enum RoleOwner {
+  OWNER = 'owner',
+}
+
+export enum RoleAdmin {
+  ADMIN = 'admin',
+}
+
 export const RoleAgentArray = ['agent'];
+export const RoleAdminArray = ['admin'];
+export const RoleOwnerArray = ['owner'];
